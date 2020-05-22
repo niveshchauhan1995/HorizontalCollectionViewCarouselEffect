e095b24765c631506a74b5c58a465c1bf522eef3
Nivesh <niveshchauhan34@gmail.com>
2020-05-22 12:45:26 +0530

    Initial Commit

---
+++ HorizontalCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HorizontalCollectionView
+//
+//  Created by Nivesh on 22/05/20.
+//  Copyright © 2020 Nivesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
