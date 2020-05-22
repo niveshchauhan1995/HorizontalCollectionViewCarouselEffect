905e130c9398ce894387636fc3e91ef1b5a2e751
Nivesh <niveshchauhan34@gmail.com>
2020-05-22 22:00:15 +0530

    Crousel Effect CollectionView Flow Layout

---
+++ HorizontalCollectionView/CarouselLayout/CarouselLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CarouselLayout.swift
+//  HorizontalCollectionView
+//
+//  Created by Nivesh on 22/05/20.
+//  Copyright © 2020 Nivesh. All rights reserved.
+//
+
+import Foundation
---
+++ HorizontalCollectionView/CollectionViewCell/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  HorizontalCollectionView
+//
+//  Created by Nivesh on 22/05/20.
+//  Copyright © 2020 Nivesh. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
